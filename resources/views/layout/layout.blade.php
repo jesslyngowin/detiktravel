0d74b0a0b7570f0275b0c2263f614fb2c4784baf
jesslyn <jesslyngwn@gmail.com>
2024-08-12 10:32:17 +0700

    add http-equiv meta tag to the layout

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> {{-- for handling mixed content error --}}
   
   <title>Detik Travel</title>
   <meta name="description" content="">
